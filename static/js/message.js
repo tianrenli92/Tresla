4ebaab11e82bd2ae8154a5c780a43fb3ddf130c4
Gabbo5915 <penggeapp2017@gmail.com>
2018-06-13 01:19:32 -0400

    message html

--- static/js/message.js
+++ static/js/message.js
@@ -1,4 +1,4 @@
-let text_box = '<div class="card-panel right" style="width: 75%; position: relative">' +
+var text_box = '<div class="card-panel right" style="width: 75%; position: relative">' +
         '<div style="position: absolute; top: 0; left:3px; font-weight: bolder" class="title">{sender}</div>' +
         '{message}' +
         '</div>';
@@ -12,7 +12,7 @@ function scrolltoend() {
 function send(sender, receiver, message) {
     $.post('/api/messages', '{"sender": "'+ sender +'", "receiver": "'+ receiver +'","message": "'+ message +'" }', function (data) {
         console.log(data);
-        let box = text_box.replace('{sender}', "You");
+        var box = text_box.replace('{sender}', "You");
         box = box.replace('{message}', message);
         $('#board').append(box);
         scrolltoend();
@@ -24,9 +24,9 @@ function receive() {
         console.log(data);
         if (data.length !== 0)
         {
-            for(let i=0;i<data.length;i++) {
+            for(var i=0;i<data.length;i++) {
                 console.log(data[i]);
-                let box = text_box.replace('{sender}', data[i].sender);
+                var box = text_box.replace('{sender}', data[i].sender);
                 box = box.replace('{message}', data[i].message);
                 box = box.replace('right', 'left blue lighten-5');
                 $('#board').append(box);
@@ -34,14 +34,4 @@ function receive() {
             }
         }
     })
-}
-
-function register(username, password) {
-    $.post('/api/users', '{"username": "'+ username +'", "password": "'+ password +'"}',
-        function (data) {
-        console.log(data);
-        window.location = '/';
-        }).fail(function (response) {
-            $('#id_username').addClass('invalid');
-        })
 }
