e024c45c18ad25f72be29d36e3e7ee62282d1bbb
Tianren Li <tianrenli92@gmail.com>
2018-07-05 15:19:52 -0400

    set aws

--- main/management/commands/createsu.py
+++ main/management/commands/createsu.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
