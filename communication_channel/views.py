341b5091b2b564eeec694f7407d94e6be4427032
Gabbo5915 <penggeapp2017@gmail.com>
2018-07-25 19:13:13 -0400

    implement single chat

--- communication_channel/views.py
+++ communication_channel/views.py
@@ -4,37 +4,41 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Message
+from project.models import Project
 from .serializers import MessageSerializer, UserSerializer
 
 
-def index(request,project_id):
+def index(request, project_id):
     if not request.user.is_authenticated:
         return redirect('login')
     if request.method == "GET":
+        project = Project.objects.get(id=project_id)
         return render(request, 'communication_channel/channel_index.html',
                       {'users': User.objects.exclude(username=request.user.username),
-                       'project_id':project_id,
+                       'project': project,
                        })
 
 
-def message_view(request, project_id, sender_id, receiver_id):
+def message_view(request, project_id, target_id):
     if request.method == "GET":
+        project = Project.objects.get(id=project_id)
+        user_id = request.user.id
         return render(request, "communication_channel/messages.html",
                       {'users': User.objects.exclude(username=request.user.username),
-                       'receiver': User.objects.get(id=receiver_id),
-                       'messages': Message.objects.filter(sender_id=sender_id, receiver_id=receiver_id) |
-                                   Message.objects.filter(sender_id=receiver_id, receiver_id=sender_id),
-                       'project_id':project_id})
+                       'target': User.objects.get(id=target_id),
+                       'messages': Message.objects.filter(sender_id=user_id, receiver_id=target_id) |
+                                   Message.objects.filter(sender_id=target_id, receiver_id=user_id),
+                       'project': project})
 
 
 @csrf_exempt
-def user_list(request, pk=None):
+def user_list(request, user_id=None):
     """
     List all required messages, or create a new message.
     """
     if request.method == 'GET':
-        if pk:
-            users = User.objects.filter(id=pk)
+        if user_id:
+            users = User.objects.filter(id=user_id)
         else:
             users = User.objects.all()
         serializer = UserSerializer(users, many=True, context={'request': request})
@@ -50,12 +54,15 @@ def user_list(request, pk=None):
 
 
 @csrf_exempt
-def message_list(request, sender=None, receiver=None):
+def message_list(request, project_id):
     """
     List all required messages, or create a new message.
     """
     if request.method == 'GET':
-        messages = Message.objects.filter(sender_id=sender, receiver_id=receiver, is_read=False)
+        sender_id = request.GET.get('sender_id', None)
+        receiver_id = request.user.id
+
+        messages = Message.objects.filter(sender_id=sender_id, receiver_id=receiver_id, is_read=False)
         serializer = MessageSerializer(messages, many=True, context={'request': request})
         for message in messages:
             message.is_read = True
@@ -69,4 +76,3 @@ def message_list(request, sender=None, receiver=None):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
-
