0f4476bb002d2482d238fdc67719db951926a5cc
Tianren Li <tianrenli92@gmail.com>
2018-11-19 15:26:01 -0500

    up to date

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    path('logout/', auth_views.logout, {'next_page': 'index'}, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True),
+        name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     path('project/', include('project.urls')),
 ]
